f6a605c023d042177d0806ae54094762aeef995e
Gabriel-Rossi-dev <costagabrielrd@gmail.com>
2025-03-31 00:50:54 -0300

    Removed using that are not in use

--- WebApiContaBancaria/Models/Transacoes/TransacoesModel.cs
+++ WebApiContaBancaria/Models/Transacoes/TransacoesModel.cs
@@ -1,6 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Drawing;
 
 namespace WebApiContaBancaria.Models.Transacoes {
     public class TransacoesModel {
